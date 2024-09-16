b24b2bcdef29c02bb2e639f29f4806ccceb085bc
room-208 <yumalegomindstorm@gmail.com>
2024-09-16 08:22:45 +0000

    add：settingsを追加

---
+++ stac-search/src/settings.py
@@ -0,0 +1,21 @@
+import os
+from datetime import datetime
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parents[2]
+
+DATA_DIR = ROOT_DIR / "data"
+DATA_ORBIT_DIR = DATA_DIR / "orbit"
+DATA_SAFE_DIR = DATA_DIR / "safe"
+DATA_STAC_DIR = DATA_DIR / "stac"
+
+
+INPUT_FILES_DIR = ROOT_DIR / "input-files"
+
+OUTPUT_DIR = ROOT_DIR / "output"
+
+STAC_API_URL = "https://catalogue.dataspace.copernicus.eu/stac"  # https://documentation.dataspace.copernicus.eu/APIs/STAC.html
+COLLECTION = "SENTINEL-1"
+USERNAME = os.getenv("COPERNICUS_USER")
+PASSWORD = os.getenv("COPERNICUS_PASSWORD")
+BBOX = [139.4099, 35.4868, 140.0562, 35.9675]  # 東京
